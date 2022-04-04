f27ece416b83d19a558a00e1fdc988b7d4a24b64
lilDev1l <lildev1l.ya@gmail.com>
2022-04-04 21:59:19 +0300

    feat: add lab4-ht-com-dll

---
+++ os/lab4-ht-com-dll/OS_0403/main.cpp
@@ -0,0 +1,66 @@
+#pragma warning(disable : 4996)
+
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <sstream>
+
+#pragma comment(lib, "../Debug/OS13_HTCOM_LIB.lib")
+#include "../OS13_HTCOM_LIB/OS13_HTCOM_LIB.h"
+
+using namespace std;
+
+string intToString(int number);
+
+int main(int argc, char* argv[])
+{
+	HANDLE hStopEvent = CreateEvent(NULL,
+		TRUE, //FALSE - автоматический сброс; TRUE - ручной
+		FALSE,
+		L"Stop");
+	setlocale(LC_ALL, "Russian");
+
+	try
+	{
+		cout << "Инициализация компонента:" << endl;
+		OS13_HTCOM_HANDEL h = OS13_HTCOM::Init();
+
+		ht::HtHandle* ht = OS13_HTCOM::HT::open(h, L"../debug/files/HTspace.ht", true);
+		if (ht)
+			cout << "-- open: success" << endl;
+		else
+			throw "-- open: error";
+
+		while (WaitForSingleObject(hStopEvent, 0) == WAIT_TIMEOUT) {
+			int numberKey = rand() % 50;
+			string key = intToString(numberKey);
+			cout << key << endl;
+
+			ht::Element* element = OS13_HTCOM::Element::createGetElement(h, key.c_str(), key.length() + 1);
+			if (OS13_HTCOM::HT::removeOne(h, ht, element))
+				cout << "-- remove: success" << endl;
+			else
+				cout << "-- remove: error" << endl;
+
+			delete element;
+
+			Sleep(1000);
+		}
+
+		OS13_HTCOM::HT::close(h, ht);
+
+		cout << endl << "Удалить компонент и выгрузить dll, если можно:" << endl;
+		OS13_HTCOM::Dispose(h);
+	}
+	catch (const char* e) { cout << e << endl; }
+	catch (int e) { cout << "HRESULT: " << e << endl; }
+
+}
+
+string intToString(int number)
+{
+	stringstream convert;
+	convert << number;
+
+	return convert.str();
+}
